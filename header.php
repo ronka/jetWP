b90572d0568d91d446b22e0f88a6c061698c99be
ronka <ronkamail@gmail.com>
2018-10-02 14:04:46 +0300

    removed title and description tag

--- header.php
+++ header.php
@@ -4,8 +4,7 @@
 <head>
   <meta charset="<?php bloginfo('charset'); ?>">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta name="description" content="<?php bloginfo('description'); ?>">
-  <title><?php wp_title(''); ?><?php if(wp_title('', false)) { echo ' :'; } ?> <?php bloginfo('name'); ?></title>
+
   <?php wp_head(); ?>
 </head>
 <body <?php body_class();?>>
